7d1b904061dc5297c162b4df7fefb21d9cb899db
abiodunobafemi <59652204+abiodunobafemi@users.noreply.github.com>
2021-02-23 18:58:19 -0500

    Create Program1.java

---
+++ Program1.java
@@ -0,0 +1,15 @@
+// Abiodun Obafemi
+
+public class Prog1_2_Obafemi {
+
+	public static void main(String[] args) {
+		
+		
+		// #1 - Display the pattern
+		System.out.println("    J     A      V     V    A");
+		System.out.println("    J    A A      V   V    A A");
+		System.out.println("J   J   AAAAA      V V    AAAAA");
+		System.out.println(" J J   A     A      V    A     A");
+	}
+
+}
